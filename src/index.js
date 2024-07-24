85b1155cad0d49666abdb0c11152c7d8b9d04d19
Chain Platform <support@chainplatform.net>
2024-07-24 12:25:18 +0700

    rebuld 1.0.5

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ export class WebView extends Component {
             }
 
             window.open(
-                (typeof source.origin_uri != "undefined" ? require(source.origin_uri + '/postMock.html') : require('./postMock.html')) +
+                require('./postMock.html') +
                 '?' +
                 Qs.stringify({
                     uri: source.uri,
